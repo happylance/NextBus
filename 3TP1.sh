af14b268bc323677533cb21a9ae87622ff6566d3
Liang Liu <nlprliu@gmail.com>
2015-12-26 22:43:45 -0500

    Use stop ID for 'PIMMIT DR + FAIRFAX TOWERS'.

--- 3TP1.sh
+++ 3TP1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-"$DIR"/nextBus.sh -s 5004590 -t 1 -d McLean -r 3T "$@"
+# PIMMIT DR + FAIRFAX TOWERS
+"$DIR"/nextBus.sh -s 5001507 -t 1 -d McLean -r 3T "$@"
 
